e1374757880d862fb2586471ca25ed52b6f07e49
C-L-STARK <x.stark.dylan@gmail.com>
2025-11-13 19:49:55 +0800

    Remove all merge conflict markers from files

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,3 @@
-import { MetadataRoute } from 'next'
-
-export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://fxkiller.com'
-
-  return {
-    rules: [
-      {
-        userAgent: '*',
-<<<<<<< HEAD
         allow: '*',
       },
       // 针对百度爬虫的特殊规则
@@ -19,35 +9,3 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: 'Googlebot',
         allow: '*',
-=======
-        allow: '/',
-        disallow: [
-          '/api/',
-          '/animation-test',
-          '/particle-test',
-          '/_next/',
-          '/admin',
-        ],
-      },
-      // 针对百度爬虫
-      {
-        userAgent: 'Baiduspider',
-        allow: '/',
-        crawlDelay: 1,
-      },
-      // 针对Google爬虫
-      {
-        userAgent: 'Googlebot',
-        allow: '/',
-      },
-      // 针对Bing爬虫
-      {
-        userAgent: 'bingbot',
-        allow: '/',
->>>>>>> fx-killer/main
-      },
-    ],
-    sitemap: `${baseUrl}/sitemap.xml`,
-    host: baseUrl,
-  }
-}
